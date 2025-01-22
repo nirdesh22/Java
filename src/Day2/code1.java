573d6ee1e8334df5443f9677967d01ca8043dc1b
nirdesh22 <nirdeshrajbanshi7@gmail.com>
2025-01-22 22:09:21 +0100

    test

---
+++ src/Day2/code1.java
@@ -0,0 +1,22 @@
+package  Day2;
+
+
+// Using the methode and giving the values from main methode to the print methode
+
+public class code1 {
+    public static void main(String[] args) {
+        print("Java ist einfach zu erlernen!");
+        print("Java ist objektorientiert!");
+        print("Java ist plattformunabhängig!");
+    }
+
+    public static void print(String s) {
+        //schreib hier deinen Code
+        //System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+
+    }
+}
