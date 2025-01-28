f4b273800a4b8d151c4e70da292d855a7b74a01f
nirdesh22 <nirdeshrajbanshi7@gmail.com>
2025-01-28 11:15:36 +0100

    done

---
+++ src/Day2/code5.java
@@ -0,0 +1,26 @@
+package Day2;
+
+public class code5 {
+        public static void main(String[] args) {
+            Frau frau = new Frau(); // using the classes below as an object here on the main methode
+            Mann mann = new Mann();
+            frau.ehemann = mann; // we created an object up above and we are saving it to this reference
+            mann.ehefrau = frau;
+
+        }
+
+        public static class Mann {
+            public int alter;
+            public int groesse;
+            public Frau ehefrau;
+        }
+
+        public static class Frau {
+            public int alter;
+            public int groesse;
+            public Mann ehemann;
+        }
+    }
+
+
+}
