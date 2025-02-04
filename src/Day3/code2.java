3cf8eb263e87a261b9bab61ff322b7af9bf8bcf0
nrajbans <nirdeshrajbanshi7@gmail.com>
2025-02-04 22:57:46 +0100

    done

---
+++ src/Day3/code2.java
@@ -0,0 +1,20 @@
+package Day3;
+
+public class code2 {
+    // to give the minimum of two values using if else statement
+        public static void main(String [] args ){
+
+            int a = 5, b = 7;
+            int c = a, d = b;
+            int m;
+            if (c>d)
+                m = c;
+            else
+                m = c;
+
+            int  min = m ;
+            System.out.println("The minimum value here is "+min);
+
+        }
+
+}
