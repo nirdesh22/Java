001b2358de98152889717027a1f6d5e0c13a95dc
nirdesh22 <nirdeshrajbanshi7@gmail.com>
2025-02-15 11:06:49 +0100

    done

---
+++ src/Day5/code1.java
@@ -0,0 +1,16 @@
+package Day5;
+
+import java.util.Scanner;
+
+public class code1 {
+    public static void main(String[] args) {
+        int airTemperature = 25;
+        int airHumidity = 60;
+        int windSpeed = 6;
+
+        // TODO: Refactor variable names and print the results
+        System.out.println("airTemperature = " + airTemperature);
+                System.out.println("airHumidity = " + airHumidity);
+        System.out.println("windSpeed = "+ windSpeed);
+    }
+}
