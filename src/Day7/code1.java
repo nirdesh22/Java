37ca7435baa17b824feee39c029c9ba7e403de08
nirdesh22 <nirdeshrajbanshi7@gmail.com>
2025-02-19 21:33:34 +0100

    done

---
+++ src/Day7/code1.java
@@ -0,0 +1,12 @@
+package Day7;
+
+public class code1 {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = a++;
+        int c = --a + b++;
+        int relsult = ++a - (c++ - b) + c;
+        System.out.println(relsult);
+    }
+}
+
