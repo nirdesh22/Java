e1706ba9a1b31ace261ec677e7beea13c97c4569
nirdesh22 <nirdeshrajbanshi7@gmail.com>
2025-02-23 16:54:14 +0100

    done

--- src/LMU/code6.java
+++ src/LMU/code6.java
@@ -1,6 +1,9 @@
 package LMU;
 
 // Extracting a substring
+// Sub-String is basically giving a certain value from a String to the another
+// Like A = apple  and B = ple
+
 
 import java.util.Scanner;
 public class code6 {
@@ -8,9 +11,11 @@ public class code6 {
         Scanner sc = new Scanner(System.in);
 
         String name = sc.nextLine();
-        int number = sc.nextInt().;
+        int number1 = sc.nextInt();
+        int number2 = sc.nextInt();
 
-        char range = number.
+        String sub = name.substring(number1, number2 +1);
 
+        System.out.println(sub);
     }
 }
